09568d3918cb238837224b41868b4f1376210129
Adrian Yong <yauvyong@gmail.com>
2018-06-24 21:14:49 +0800

    Linked to mysql
    Linked to mysql.
    Models function working properly.

---
+++ www/app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Adrian Yong"
+
+import logging; logging.basicConfig(level=logging.INFO)
+import asyncio, os, json, time
+from datetime import datetime
+from aiohttp import web
+
+def index(request):
+	return web.Response(body=b'<h1>Awesome<h4/h1>')
+	
+@asyncio.coroutine
+def init(loop):
+	app=web.Application(loop=loop)
+	app.router.add_route('GET','/',index)
+	srv = yield from loop.create_server(app.make_handler(),'0.0.0.0', 9000)
+	logging.info('server started at http://0.0.0.0:9000...')
+	
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
