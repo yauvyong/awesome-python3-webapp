c70dd2e230e986691f07d7f5eb45ecdd45f74823
Adrian Yong <yauvyong@gmail.com>
2018-06-25 23:06:50 +0800

    Share
    coroweb.py - full

--- www/coroweb.py
+++ www/coroweb.py
@@ -74,7 +74,7 @@ def has_request_args(fn):
 			raise ValueError('request parameter must be the last named parameter in function: %s%s' % (fn.__name__, str(sig))
 	return found
 	
-class requestHandler(object):
+class RequestHandler(object):
 	def __init__(self, app, fn):
 		self.app = app
 		self.fn = fn
@@ -118,10 +118,13 @@ class requestHandler(object):
 					if name in kw:
 						copy[name] = kw[name]
 				kw=copy
+			#(kw['request'] = request) same with (request.match_info.items())?
 			for k, v in request.match_info.items():
 				if k in kw:
 					logging.warning('Duplicate arg name in named arg and kw args: %s' % k)
 				kw[k] = v
+				
+		#Check the kw match with require args, return bad request if not same
 		if self._has_request_args:
 			kw['request'] = request
 		if self._required_kw_args:
@@ -136,7 +139,34 @@ class requestHandler(object):
 			return dict(error=e.error, data=e.data, message=e.message)
 		
 	
+	def add_static(app):
+		path = os.path.join(os.path.driname(os.path.abspath(__file__)), 'static')
+		app.router.add_static('/static/',path)
+		logging.info('add static %s => %s' % ('/static/', path))
+		
 	def add_route(app, fn):
 		method = getattr(fn, '__method__', None)
 		path = getattr(fn, '__route__', None)
-		
+		if path is None or method is None:
+			raise ValueError('@get or @post not defined in%s.' % str(fn))
+		if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
+			fn = asyncio.coroutine(fn)
+		logging.info('add route %s %s => %s(%s)' %(method, path, fn.__name__, ', '.joain(inspect.signature(fn).parameters.keys())))
+		app.router.add_route(method, path, RequestHandler(app,fn))
+	
+	def add_routes(app, module_name):
+		n = module_name.rfind('.')
+		if n==(-1):
+			mod = __import__(module_name,globals(),locals())
+		else:
+			name = module_name[n+1]
+			mod = getattr(__import__(module_name[:n], globals(), locals(),[name]), name)
+		for attr in dir(mod):
+			if attr.startswith('_'):
+				continue
+			fn = getattr(mod,attr)
+			if callable(fn):
+				method = getattr(fn, '__method__', None)
+				path = getattr(fn, '__route__', None)
+				if method and path:
+					add_route(app, fn)
