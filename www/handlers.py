22e4036213ca0911d8c7952443224b578443115f
Adrian Yong <yauvyong@gmail.com>
2018-06-26 16:42:30 +0800

    Share
    handlers.py
    templates\test.html

--- www/handlers.py
+++ www/handlers.py
@@ -4,3 +4,19 @@
 __author__ = 'Adrian Yong'
 
 'url handlers'
+
+import re, time, json, logging, hashlib,base64 asyncio
+
+from coroweb import get, post
+from models import USer, Comment, Blog, next_id
+
+@get('/')
+@asyncio.coroutine
+def index(request):
+	users = yield from User.findAll()
+	
+	return {
+		'__template__': 'test.html',
+		'users': users
+		}
+		
