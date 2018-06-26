6409b57dec62bb1acc83b2db683d553804e716c3
Adrian Yong <yauvyong@gmail.com>
2018-06-26 12:48:55 +0800

    Share
    Day 5 correction

--- www/app.py
+++ www/app.py
@@ -15,7 +15,7 @@ from coroweb import add_routes, add_static
 def init_jinja2(app, **kw):
 	logging.info('init jinja2...')
 	options = dict(
-		sutoescape = kw.get('autoescape', True),
+		autoescape = kw.get('autoescape', True),
 		block_start_string = kw.get('block_start_string', '{%'),
 		variable_start_string = kw.get('variable_start_string', '{{'),
 		variable_end_string = kw.get('variable_end_string', '}}'),
@@ -23,13 +23,13 @@ def init_jinja2(app, **kw):
 	)
 	path = kw.get('path', None)
 	if path is None:
-		path.os.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+		path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 	logging.info('set jinja2 template path : %s' %path)
-	env = Enviroment(loader=FileSystemLoader(path),**options)
+	env = Environment(loader=FileSystemLoader(path),**options)
 	filters = kw.get('filters', None)
-	if fileter is not None:
+	if filters is not None:
 		for name, f in filters.items():
-			env.filters[name] = file
+			env.filters[name] = f
 	app['__templating__'] = env
 	
 @asyncio.coroutine
@@ -94,7 +94,7 @@ def response_factory(app, handler):
 		return resp
 	return response
 
-def datetime_fileter(t):
+def datetime_filter(t):
 	delta = int(time.time() - t)
 	if delta < 60:
 		return u'1 minute before'
---
+++ www/handlers.py
@@ -0,0 +1,6 @@
+﻿#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Adrian Yong'
+
+'url handlers'
