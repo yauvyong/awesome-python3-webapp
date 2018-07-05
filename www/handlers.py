6430270e9689d7c6f4fd9083223aea305ef8bd04
Adrian Yong <yauvyong@gmail.com>
2018-07-05 17:59:41 +0800

    upload to main PC

--- www/handlers.py
+++ www/handlers.py
@@ -20,6 +20,10 @@ _COOKIE_KEY = configs.session.secret
 _RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[[a-z0-9\-\_]+){1,4}$')
 _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
 
+def check_admin(request):
+	if request.__user__ is None or not request.__user__.admin:
+		raise APIPermissionError()
+		
 def user2cookies(user, max_age):
 	#build string by : id-expires-sha1
 	expires = str(int(time.time() + max_age))
@@ -67,6 +71,41 @@ def index(request):
 		'user': request.__user__
 		}
 
+@get('/signout')
+def signout(request):
+	referer = request.headers.get('Referer')
+	r = web.HTTPFound(referer or '/')
+	r.set_cookie(COOKIE_NAME,'-deleted-',max_age=0, httponly=True)
+	logging.info('user signed out')
+	return r
+	
+@get('/signin')
+@asyncio.coroutine		
+def signin(request):
+	return {
+		'__template__': 'signin.html'
+		}
+		
+@get('/register')
+@asyncio.coroutine		
+def register(request):
+	return {
+		'__template__': 'register.html'
+		}
+
+@get('/manage/blogs/create')
+def manage_create_blog():
+	return {
+		'__template__': 'manage_blog_edit.html',
+		'id': ='',
+		'action': '/api/blogs'
+	}
+	
+@get('api/blogs/{id}')
+def api_get_blog(*,id):
+	blog = yield from Blog.find(id)
+	return blog
+		
 @post('/api/users')
 @asyncio.coroutine	
 def api_register_user(*,email,name,passwd):
@@ -91,28 +130,6 @@ def api_register_user(*,email,name,passwd):
 	r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
 	return r
 	
-@get('/signout')
-def signout(request):
-	referer = request.headers.get('Referer')
-	r = web.HTTPFound(referer or '/')
-	r.set_cookie(COOKIE_NAME,'-deleted-',max_age=0, httponly=True)
-	logging.info('user signed out')
-	return r
-	
-@get('/signin')
-@asyncio.coroutine		
-def signin(request):
-	return {
-		'__template__': 'signin.html'
-		}
-		
-@get('/register')
-@asyncio.coroutine		
-def register(request):
-	return {
-		'__template__': 'register.html'
-		}
-
 @post('/api/authenticate')
 @asyncio.coroutine	
 def authenticate(*, email, passwd):
@@ -138,3 +155,18 @@ def authenticate(*, email, passwd):
 	r.content_type = 'application/json'
 	r.body = json.dumps(user,ensure_ascii=False).encode('utf-8')
 	return r
+
+@post('/api/blogs')
+@asyncio.coroutine
+def api_create_blog(request, *, name, summary, content):
+	check_admin(request)
+	if not name or not name.strip():
+		raise APIValueError('name', 'name cannot be empty')
+	if not summary or not summary.strip():
+		raise APIValueError('summary', 'summary cannot be empty')
+	if not content or not content.strip():
+		raise APIValueError('content', 'content cannot be empty')
+	blog = Blog(user_id=request.__user__.id, user_name=request.__user__.name, user_image = request.__user__.image, name=name.strip(), summary=summary.strip(),content=content.strip())
+	yield from blog.save()
+	return blog
+	
