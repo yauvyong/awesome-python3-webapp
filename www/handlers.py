b6a3b7a77d04cf61d402b6330a1b81a57ce49cae
Adrian Yong <yauvyong@gmail.com>
2018-07-09 13:06:23 +0800

    test edit

--- www/handlers.py
+++ www/handlers.py
@@ -103,7 +103,7 @@ def manage_create_blog(request):
 	}
 
 @get('/manage/blogs/edit/{id}')
-def manage_create_blog(*,id,request):
+def manage_edit_blog(*,id,request):
 	return {
 		'__template__': 'test.html',
 		'id': id,
