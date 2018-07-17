28fd544d925c02aabd101f50d65b8458d2ca284e
Adrian Yong <yauvyong@gmail.com>
2018-07-17 11:47:57 +0800

    manage

--- www/app.py
+++ www/app.py
@@ -108,7 +108,7 @@ def auth_factory(app,handler):
 			if user:
 				logging.info('set current user: %s' % user.email)
 				request.__user__=user
-			if request.path.startswith('/manage/' and (request.__user__ is None or not request.__user__.admin):
+			if request.path.startswith('/manage/') and (request.__user__ is None or not request.__user__.admin):
 				return web.HTTPFound('/singin')
 		return (yield from handler(request))
 	return auth
