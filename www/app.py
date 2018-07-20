fb985b27070dc3ebb36fee027f65d4d893852d13
Adrian Yong <yauvyong@gmail.com>
2018-07-20 16:51:08 +0800

    test

--- www/app.py
+++ www/app.py
@@ -60,47 +60,38 @@ def response_factory(app, handler):
 	@asyncio.coroutine
 	def response(request):
 		logging.info('Response handler...')
-		r = yield from handler(request)
 		if isinstance(r, web.StreamResponse):
-			logging.info('a')
 			return r
 		if isinstance(r, bytes):
 			resp = web.Response(body=r)
 			resp.content_type = 'application/octect-stream'
-			logging.info('b')
 			return resp
 		if isinstance(r, str):
 			if r.startswith('redirect:'):
 				return web.HttpFound(r[9:])
 			resp = web.Response(body=e.encode('utf-8'))
 			resp.content_type = 'text/html;charset=utf-8'
-			logging.info('c')
 			return resp
 		if isinstance(r, dict):
 			template = r.get('__template__')
 			if template is None:
 				resp = web.Response(body=json.dumps(r, ensure_ascii=False,default=lambda o:o.__dict__).encode('utf-8'))
 				resp.content_type = 'application/json;charset=utf-8'
-				logging.info('d')
 				return resp
 			else:
 				r['__user__'] = request.__user__
 				resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
 				resp.content_type = 'text/html;charset=utf-8'
-				logging.info('e')
 				return resp
 		if isinstance(r, int) and r>=100 and r<=600:
-			logging.info('f')
 			return response(r)
 		if isinstance(r, tuple) and len(r) ==2:
 			t,m = r
 			if isinstance(t, int) and t>=100 and t<=600:
-				logging.info('g')
 				return web.Response(t, str(m))
 		#default
 		resp = web.Response(body=str(r).encode('utf-8'))
 		resp.content_type = 'text/plain;charset=utf-8'
-		logging.info('h')
 		return resp
 	return response
 
