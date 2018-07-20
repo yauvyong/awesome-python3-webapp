e2e01b5f0341309de3ac3e463d3fc87e6fee248c
Adrian Yong <yauvyong@gmail.com>
2018-07-20 16:09:47 +0800

    4044

--- www/app.py
+++ www/app.py
@@ -89,7 +89,7 @@ def response_factory(app, handler):
 		if isinstance(r, tuple) and len(r) ==2:
 			t,m = r
 			if isinstance(t, int) and t>=100 and t<=600:
-				return web.Response(t, str(m), "error")
+				return web.Response(t, str("ass"))
 		#default
 		resp = web.Response(body=str(r).encode('utf-8'))
 		resp.content_type = 'text/plain;charset=utf-8'
