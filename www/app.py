872ba6f098e920adc1e4dfabf60e9e51dd971bd0
Adrian Yong <yauvyong@gmail.com>
2018-07-20 15:49:29 +0800

    404

--- www/app.py
+++ www/app.py
@@ -114,7 +114,7 @@ def auth_factory(app,handler):
 	return auth
 
 def handle404(app, error):
-    template = env.get_template('404.html')
+    template = app.get_template('404.html')
     return template.render()
 
 def datetime_filter(t):
